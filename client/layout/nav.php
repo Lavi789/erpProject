27f47177b6f3309df2aa0670dd97ad210af1a81e
Lavanya <53055984+Lavi789@users.noreply.github.com>
2024-04-24 17:09:17 +0530

    ADDED

--- client/layout/nav.php
+++ client/layout/nav.php
@@ -167,48 +167,7 @@ function getActive($amenu, $tmenu)
                 </li>
             </ul>
         </li>
-        <li>
-            <a href="javascript:;" class="top-menu <?php getActive($amenu, "sales"); ?>">
-                <div class="top-menu__icon"> <i data-lucide="shopping-cart"></i> </div>
-                <div class="top-menu__title"> Sales <i data-lucide="chevron-down" class="top-menu__sub-icon"></i> </div>
-            </a>
-            <ul class="">
-                <li>
-                    <a href="retail_sales.php" class="top-menu">
-                        <div class="top-menu__icon"> <i data-lucide="plus-circle"></i> </div>
-                        <div class="top-menu__title"> Retail Sales </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="sales_return.php" class="top-menu">
-                        <div class="top-menu__icon"> <i data-lucide="corner-down-left"></i> </div>
-                        <div class="top-menu__title"> Sales Return </div>
-                    </a>
-                </li>
-            </ul>
-        </li>
-
-        <li>
-            <a href="javascript:;" class="top-menu <?php getActive($amenu, "purchase"); ?>">
-                <div class="top-menu__icon"> <i data-lucide="activity"></i> </div>
-                <div class="top-menu__title"> Purchase <i data-lucide="chevron-down" class="top-menu__sub-icon"></i> </div>
-            </a>
-            <ul class="">
-                <li>
-                    <a href="purchase_inv.php" class="top-menu">
-                        <div class="top-menu__icon"> <i data-lucide="credit-card"></i> </div>
-                        <div class="top-menu__title"> Purchase Invoice </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="purchase_return.php" class="top-menu">
-                        <div class="top-menu__icon"> <i data-lucide="corner-down-left"></i> </div>
-                        <div class="top-menu__title"> Purchase Return </div>
-                    </a>
-                </li>
-            </ul>
-        </li>
-
+        
         <li>
             <a href="javascript:;" class="top-menu <?php getActive($amenu, "report"); ?>">
                 <div class="top-menu__icon"> <i data-lucide="bar-chart"></i> </div>
@@ -218,19 +177,37 @@ function getActive($amenu, $tmenu)
                 <li>
                     <a href="stockreport.php" class="top-menu">
                         <div class="top-menu__icon"> <i data-lucide="file-text"></i> </div>
-                        <div class="top-menu__title"> Stock Report </div>
+                        <div class="top-menu__title"> Party List  </div>
                     </a>
                 </li>
                 <li>
                     <a href="sales_report.php" class="top-menu">
                         <div class="top-menu__icon"> <i data-lucide="file-text"></i> </div>
-                        <div class="top-menu__title"> Date Wise Sales Report </div>
+                        <div class="top-menu__title"> Group wise Item List </div>
                     </a>
                 </li>
                 <li>
                     <a href="purchase_report.php" class="top-menu">
                         <div class="top-menu__icon"> <i data-lucide="file-text"></i> </div>
-                        <div class="top-menu__title"> Date Wise Purchase Report </div>
+                        <div class="top-menu__title"> Vendor Item List  </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="purchase_report.php" class="top-menu">
+                        <div class="top-menu__icon"> <i data-lucide="file-text"></i> </div>
+                        <div class="top-menu__title"> Production BOM  </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="purchase_report.php" class="top-menu">
+                        <div class="top-menu__icon"> <i data-lucide="file-text"></i> </div>
+                        <div class="top-menu__title"> Vendor Item List  </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="purchase_report.php" class="top-menu">
+                        <div class="top-menu__icon"> <i data-lucide="file-text"></i> </div>
+                        <div class="top-menu__title"> Job Contracter BOM  </div>
                     </a>
                 </li>
             </ul>
