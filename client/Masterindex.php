e6d085c4902d9463067ea94de1457fb67da4d6f5
Lavanya <53055984+Lavi789@users.noreply.github.com>
2024-05-13 13:14:42 +0530

    added

--- client/Masterindex.php
+++ client/Masterindex.php
@@ -24,6 +24,9 @@ require_once '../server/config/db.php';
 </head>
 <!-- END: Head -->
 
+
+      
+
 <body class="py-5 md:py-0">
 
     <!-- BEGIN: Mobile Menu Menu -->
@@ -56,65 +59,10 @@ require_once '../server/config/db.php';
             </h2>
             <a href="" class="ml-auto flex items-center text-primary"> <i data-lucide="refresh-ccw" class="w-4 h-4 mr-3"></i> Reload Data </a>
         </div>
-        <!-- <div class="grid grid-cols-12 gap-6 mt-5">
-            <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-                <div class="report-box zoom-in">
-                    <div class="box p-5">
-                        <div class="flex">
-                            <i data-lucide="shopping-cart" class="report-box__icon text-primary"></i>
-                            <div class="ml-auto">
-                                <div class="report-box__indicator bg-success tooltip cursor-pointer" title="33% Higher than last month"> 33% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
-                            </div>
-                        </div>
-                        <div class="text-3xl font-medium leading-8 mt-6">4.710</div>
-                        <div class="text-base text-slate-500 mt-1">Item Sales</div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-                <div class="report-box zoom-in">
-                    <div class="box p-5">
-                        <div class="flex">
-                            <i data-lucide="credit-card" class="report-box__icon text-pending"></i>
-                            <div class="ml-auto">
-                                <div class="report-box__indicator bg-danger tooltip cursor-pointer" title="2% Lower than last month"> 2% <i data-lucide="chevron-down" class="w-4 h-4 ml-0.5"></i> </div>
-                            </div>
-                        </div>
-                        <div class="text-3xl font-medium leading-8 mt-6">3.721</div>
-                        <div class="text-base text-slate-500 mt-1">New Orders</div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-                <div class="report-box zoom-in">
-                    <div class="box p-5">
-                        <div class="flex">
-                            <i data-lucide="monitor" class="report-box__icon text-warning"></i>
-                            <div class="ml-auto">
-                                <div class="report-box__indicator bg-success tooltip cursor-pointer" title="12% Higher than last month"> 12% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
-                            </div>
-                        </div>
-                        <div class="text-3xl font-medium leading-8 mt-6">2.149</div>
-                        <div class="text-base text-slate-500 mt-1">Total Products</div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-span-12 sm:col-span-6 xl:col-span-3 intro-y">
-                <div class="report-box zoom-in">
-                    <div class="box p-5">
-                        <div class="flex">
-                            <i data-lucide="user" class="report-box__icon text-success"></i>
-                            <div class="ml-auto">
-                                <div class="report-box__indicator bg-success tooltip cursor-pointer" title="22% Higher than last month"> 22% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
-                            </div>
-                        </div>
-                        <div class="text-3xl font-medium leading-8 mt-6">152</div>
-                        <div class="text-base text-slate-500 mt-1">Customer</div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-        <!-- END: General Report -->
+        
+    <!-- Element to animate -->
+   <!-- Animated box -->
+   <div class="watermark-container"></div>
     </div>
     <!-- END: Content -->
 
